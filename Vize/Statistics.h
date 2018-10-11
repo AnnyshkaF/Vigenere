5b2b11f40b54ca669779ea0693bf61629f21befe
Anna Fominykh <36913914+AnnyshkaF@users.noreply.github.com>
2018-10-11 22:46:39 +0300

    Update Statistics.h

--- Vize/Statistics.h
+++ Vize/Statistics.h
@@ -9,13 +9,13 @@ public:
 	std::map<unsigned char, int> alpha;
 	unsigned char alphabet[256];
 
-									Statistics();
-									~Statistics();
+					Statistics();
+					~Statistics();
 	std::map<unsigned char, int>	CalculateStatistics(std::vector<unsigned char> text);
 	std::map<unsigned char, int>	CalculateStatisticsInOrder(std::vector<unsigned char> text);
-	double							ÑalculateIndexOfCoincidence(std::vector<unsigned char> text);
-	unsigned char					getLetter(int number);
-	void							ShowStatistics(std::map<char, int> map);
-	int								getLetterNumber(unsigned char letter);
+	double				CalculateIndexOfCoincidence(std::vector<unsigned char> text);
+	unsigned char			getLetter(int number);
+	void				ShowStatistics(std::map<char, int> map);
+	int				getLetterNumber(unsigned char letter);
 };
 
